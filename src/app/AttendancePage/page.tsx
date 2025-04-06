d720cf90efda2d2f32154caeaa4501b8079cb3b6
yogesh567894 <yogeshwaran310320005@gmail.com>
2025-04-06 17:21:26 +0530

    Refactor AttendancePage to remove unused CardHeader and CardTitle imports

--- src/app/AttendancePage/page.tsx
+++ src/app/AttendancePage/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 // <<<<<<< HEAD
